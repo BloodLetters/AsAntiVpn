9d093b715ed755f21f5b0a7d2fb45e2424c8fb6e
BloodLetters <BloodLetters45@gmail.com>
2024-05-29 15:37:05 +0800

    Init Project

---
+++ src/main/java/me/ashesh/AsAntiVpn.java
@@ -0,0 +1,17 @@
+package me.ashesh;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AsAntiVpn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/ashesh/Event/joinEvent.java
@@ -0,0 +1,4 @@
+package me.ashesh.Event;
+
+public class joinEvent {
+}
