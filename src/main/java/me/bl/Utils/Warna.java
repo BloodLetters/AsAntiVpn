e1ff9abeda58d2462707a1af7bcdb61028db0222
Ashesh <103701190+BloodLetters@users.noreply.github.com>
2024-05-29 15:38:20 +0800

    Delete src/main directory

--- src/main/java/me/bl/Utils/Warna.java
+++
@@ -1,9 +0,0 @@
-package me.bl.Utils;
-
-import org.bukkit.ChatColor;
-
-public class Warna {
-    public static String color(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
-    }
-}
